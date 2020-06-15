30e6e20601d56b6274e3254b5fcf7ab26ba58132
Cansu <cansu96yildiz@gmail.com>
2020-06-16 02:26:37 +0300

    projects view updated

--- javascripts/gh-cards.js
+++ javascripts/gh-cards.js
@@ -47,7 +47,7 @@ let style = `
     font-family: 'Arial';
     box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     transition: 0.3s;
-    width: 90%;
+    width: 80%;
     text-align: center;
     display: block;
     margin-left: auto;
